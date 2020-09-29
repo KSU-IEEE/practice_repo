e24a9dd0e70e9f01f34b5d81aa63347a442ec655
Noah Trinite <trinitenoah@gmail.com>
2020-09-28 21:17:10 -0400

    CMake stuff

---
+++ modules/cpp/build/CMakeFiles/no_header.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/no_header.dir/src/no_header.cpp.o"
+  "no_header.pdb"
+  "no_header"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/no_header.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
